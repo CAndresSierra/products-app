f26327a5c4456bb00c5530fccd39aec675456e96
CAndresSierra <camilofullstack@gmail.com>
2026-05-27 11:44:21 -0500

    feat(config): setup environment validation with joi
    add joi dependency, create env validation schema, and update main.ts to use validated env variables

--- products-ms/src/main.ts
+++ products-ms/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,8 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true
   }))
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+  console.log(`Application is running on: http://localhost:${envs.port}`);
+
 }
 bootstrap();
